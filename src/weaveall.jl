13513ce0a03c7ecf71fbe38d7e36fd5dc8808d43
ashton.bradley@otago.ac.nz <ashton.bradley@gmail.com>
2019-04-18 19:47:46 +1200

    soliton

--- src/weaveall.jl
+++ src/weaveall.jl
@@ -9,13 +9,13 @@ html_path = joinpath(source_path,"../docs","html")
 pdf_path = joinpath(source_path,"../docs","pdf")
 notebook_path = joinpath(source_path,"../docs","notebooks")
 
-source_list = ["1dharmonic"]
+source_list = ["1dharmonic" "1dbrightsoliton"]
 
 function weavedocs(file)
     tmp = joinpath(source_path,file)*".jmd"
     weave(tmp,out_path=html_path,doctype = "md2html")
-    weave(tmp,out_path=pdf_path,doctype="md2pdf"; template=latexfile)
-    Weave.convert_doc(tmp,joinpath(notebook_path,file)*".ipynb")
+    # weave(tmp,out_path=pdf_path,doctype="md2pdf"; template=latexfile)
+    # Weave.convert_doc(tmp,joinpath(notebook_path,file)*".ipynb")
 end
 
 for file ∈ source_list
