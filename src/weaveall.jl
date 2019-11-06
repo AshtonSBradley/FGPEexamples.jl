fda9dc7b28a43e2ae36f762fbda4102564655d53
AshtonSBradley <ashton.bradley@gmail.com>
2019-11-06 14:33:14 +1300

    update .gif links

--- src/weaveall.jl
+++ src/weaveall.jl
@@ -10,7 +10,7 @@ pdf_path = joinpath(source_path,"../docs","pdf")
 notebook_path = joinpath(source_path,"../docs","notebooks")
 
 # flist = ["1dharmonic" "1dbrightsoliton" "2dharmonic" "jrsoliton"]
-flist = ["2dharmonic"]
+flist = ["1dbrightsoliton"]
 
 function weavedocs(file)
     tmp = joinpath(source_path,file)*".jmd"
