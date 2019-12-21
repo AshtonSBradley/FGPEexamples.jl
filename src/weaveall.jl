a4402cb83bac30af16b5fce3818db8a96e50a1fb
ashton.bradley@otago.ac.nz <ashton.bradley@gmail.com>
2019-12-22 09:48:49 +1300

    add the classic GPE periodic soliton

--- src/weaveall.jl
+++ src/weaveall.jl
@@ -12,7 +12,8 @@ html_path = joinpath(source_path,"../docs","html")
 pdf_path = joinpath(source_path,"../docs","pdf")
 notebook_path = joinpath(source_path,"../docs","notebooks")
 
-flist = ["1dharmonic" "1dbrightsoliton" "2dharmonic" "3dquenchiso" "3dquenchtrap" "3dquenchslab" "3dquenchtube"] # "jrsoliton"]
+# flist = ["1dharmonic" "1dbrightsoliton" "2dharmonic" "3dquenchiso" "3dquenchtrap" "3dquenchslab" "3dquenchtube" "jrsoliton" "1dtemporalsoliton"]
+flist = ["1dtemporalsoliton"]
 
 function weavedocs(file)
     tmp = joinpath(source_path,file)*".jmd"
@@ -22,9 +23,11 @@ function weavedocs(file)
 end
 
 function makeall(flist)
-for file ∈ flist
+for file in flist
     weavedocs(file)
 end
 end
 
 makeall(flist)
+
+Pkg.activate()
